e6628d92497b85d6d01fff844e4b431868c1006f
eulptr <e.skelaite@gmail.com>
2024-03-19 16:28:45 +0200

    1-5

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,65 @@ public class Main {
         int a = 19;
         int b = 3;
         suma(10, 20);
-        suma(a,b);
+        suma(a, b);
 
+        System.out.println("<2 uzduotis>");
         System.out.println(Pisq());
+
+        System.out.println("<4 uzduotis>");
+        int[] arr = {6, 5, 7, 8};
+        printArr(arr);
+
+        System.out.println("<3 uzduotis>");
+        int c = 4;
+        int d = 4;
+        System.out.println(sandauga(c,d));
+
+        System.out.println("<5 uzduotis>");
+        int e = 0;
+        int f = 30;
+        System.out.println(random(e,f));
+
+        System.out.println("<6 uzduotis>");
+        int [] randArr = randArr ();
+        printArr(randArr);
+
     }
-    public static void suma(int a, int b){
-        int suma = a+b;
-        System.out.println(suma);
+
+    public static void suma(int a, int b) {
+        int suma = a + b;
+        System.out.println(suma); //1 uzduotis
     }
 
     public static double Pisq() {
-        return 9.8596;
+        return 9.8596; //2 uzduotis
     }
 
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " "); //4 uzduotis
+        }
+        System.out.println();
+    }
 
-    
+    public static int sandauga(int c, int d) {
+        int sandauga = (c * d);
+        return sandauga; //3 uzduotis
+
+    }
+
+    public static int random (int e, int f){
+        return (int) (e + Math.random() * (f-e)); //5 uzduotis
+    }
+
+public static int randomArr (int min, int max, int length) {
+        int [] randArray;
+    for (int g = 0; g < length; g++) {
+        randomArr[g] = min - (Math.random()) * (max - min);
+
+    }
+}
+
+
+
 }
