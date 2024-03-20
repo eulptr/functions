be45d8ad520475c94ccc900c5e90982d07bcc144
eulptr <e.skelaite@gmail.com>
2024-03-20 16:13:38 +0200

    1-11

--- src/Main.java
+++ src/Main.java
@@ -16,17 +16,39 @@ public class Main {
         System.out.println("<3 uzduotis>");
         int c = 4;
         int d = 4;
-        System.out.println(sandauga(c,d));
+        System.out.println(sandauga(c, d));
 
         System.out.println("<5 uzduotis>");
         int e = 0;
         int f = 30;
-        System.out.println(random(e,f));
+        System.out.println(random(e, f));
 
         System.out.println("<6 uzduotis>");
-        System.out.println(Arr(5, 10, 15));
+        int[] arr2 = arr(5, 10, 15);
+        printArr(arr2);
+
+        System.out.println("<7 uzduotis>");
+        int[] arr3 = arr(5, 10, 15);
+        int sum = arrSum(arr3);
+        System.out.println(sum);
+
+        System.out.println("<8 uzduotis>");
+        int averageArr = arrSum(arr) / arr.length;
+        System.out.println(averageArr);
+        System.out.println(averageArr(arr));
+
+        System.out.println("<9 uzduotis>");
+        printsquare(5, 5);
+
+        System.out.println("<10 uzduotis>");
+        String sentence = "Siandien labai grazi diena";
+        encodeText(sentence);
+
+        System.out.println("<11 uzduotis>");
+        System.out.println(reverseString("Eulalija"));
 
     }
+
 
     public static void suma(int a, int b) {
         int suma = a + b;
@@ -50,18 +72,67 @@ public class Main {
 
     }
 
-    public static int random (int e, int f){
-        return (int) (e + Math.random() * (f-e)); //5 uzduotis
+    public static int random(int e, int f) {
+        return (int) (e + Math.random() * (f - e)); //5 uzduotis
     }
 
-public static int[] Arr (int min, int max, int length) {
-        int [] array = new int[length];
-    for (int g = 0; g < length; g++) {
-        array [g] = min - (int)Math.round(Math.random()) * (max - min);
+    public static int[] arr(int min, int max, int length) {
+        int[] array = new int[length];
+        for (int g = 0; g < length; g++) {
+            array[g] = min - (int) Math.round(Math.random() * (max - min));
+        }
+        return array; //6 uzduotis
     }
-    return array;
-}
+
+    public static int arrSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum; //7 uzduotis
+    }
+
+    public static double averageArr(int[] arr) {
+        return (double) arrSum(arr) / arr.length; //8 uzduotis
+    }
+
+    public static void printsquare(int num1, int num2) {
+        for (int i = 0; i < num1; i++) {
+            for (int j = 0; j < num2; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); //9 uzduotis
+        }
+    }
+
+    public static void encodeText(String sentence) {
+        int countSpaces = 0;
+        int countLetters = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            sentence.charAt(i);
+            if (sentence.charAt(i) == ' ') {
+                countSpaces++;
+            } else {
+                countLetters++;
+            }
+        }
+        System.out.println("Tekste yra " + countSpaces + " tarpu");
+        System.out.println("Tekste yra " + countLetters + " raidziu");
+    }
+
+    public static String reverseString(String input) {
+        char[] inputArray = input.toCharArray();
+        char[] reverseArray = new char [inputArray.length];
+
+        for (int i = 0; i < inputArray.length; i++) {
+            reverseArray[i] = inputArray[inputArray.length -1 - i];
+        }
+        String reversedString = new String (reverseArray);
+        return reversedString;
 
 
 
-}
+
+        }
+    }
+
